59823b942f749e46f004005ba1a6269326b1b976
Yuske Eduardo Velit Arruda <yuskevelit@hotmail.com>
2025-01-22 09:41:38 -0300

    init

---
+++ src/Kai/Controllers/DynamicQueryParamsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kai.Controllers;
+
+[ApiController]
+[Route("api/dynamic-query-params")]
+public class DynamicQueryParamsController : ControllerBase
+{
+    [HttpGet]
+    public IResult Get([FromQuery] IDictionary<string, string[]> queryParams)
+    {
+        return Results.Ok(queryParams);
+    }
+}
